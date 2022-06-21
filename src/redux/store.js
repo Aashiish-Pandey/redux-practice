2821615c70b4e54113907ac4f7d14823b111e76b
Aashiish-Pandey <ash006pandey@gmail.com>
2022-06-21 12:26:51 +0530

    logger middleware and redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,8 @@
-import {createStore} from 'redux';
+import {createStore,applyMiddleware} from 'redux';
 import rootreducer from './rootReducer';
-const store = createStore(rootreducer)
+import{logger} from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension';
+const store = createStore(rootreducer,composeWithDevTools(applyMiddleware(logger)))
 
 console.log(store.getState())
 export default store
